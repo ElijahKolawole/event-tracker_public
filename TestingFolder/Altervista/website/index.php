8401be1938bb98a1345afe2ec75561e3eae85cbc
Yiet Mai <ymai@ggc.edu>
2018-10-22 18:30:30 -0400

    Yiet removing unused Altervista folder

--- TestingFolder/Altervista/website/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$page_title = 'Welcome to Event Tracking';
-include ('header.html');
-?>
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%2333ccff&amp;src=8km251e1tgiu5j8521mava1gn4%40group.calendar.google.com&amp;color=%23853104&amp;ctz=America%2FNew_York" style="border:solid 1px #777" width="100%" height="500" frameborder="0" scrolling="no"></iframe>                    
-<?php
-
-include ('footer.html');
-?>
